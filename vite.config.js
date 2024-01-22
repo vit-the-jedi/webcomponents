ef27ab4063bfce65ec0cd8f48e2591cb7658c80c
Matt Vitello <Matt@Matts-MacBook-Pro-2.local>
2024-01-22 11:08:23 -0500

    build

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     minify: true,
     outDir: "./dist",
     rollupOptions: {
-      input: ['components/progress/progress-steps/init-steps.js','components/progress/progress-bar/init-bar.js','components/progress/globals/classes/progress.js','components/progress/progress-bar/modules/classes/progress-bar.js', 'components/progress/progress-steps/modules/classes/progress-steps.js'],
+      input: ['components/progress/progress-circle/init-circle.js','components/progress/progress-steps/init-steps.js','components/progress/progress-bar/init-bar.js','components/progress/globals/classes/progress.js','components/progress/progress-bar/modules/classes/progress-bar.js', 'components/progress/progress-steps/modules/classes/progress-steps.js', 'components/progress/progress-circle/modules/classes/progress-circle.js'],
       output: {
         entryFileNames: `[name].js`,
       },
