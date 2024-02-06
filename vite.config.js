5a61733128b6aadedbf0978aac9f07bf7e46e0f5
Matt Vitello <Matt@Matts-MacBook-Pro-2.local>
2024-02-06 11:32:54 -0500

    update vite config to dynamically grab all files for build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,37 @@
 "use strict";
 
 import { defineConfig } from "vite";
+import * as fs from "node:fs/promises";
+import path from "path";
+
+const walk = async (dir, filelist = []) => {
+  const files = await fs.readdir(dir);
+
+  for (const file of files) {
+    if (file[0] !== ".") {
+      const filepath = path.join(dir, file);
+      if (!filepath.includes("tests")) {
+        const stat = await fs.stat(filepath);
+
+        if (stat.isDirectory()) {
+          filelist = await walk(filepath, filelist);
+        } else {
+          filelist.push(filepath);
+        }
+      }
+    }
+  }
+  return filelist;
+};
+
+let inputFileArr = await walk("./components/progress/");
 
 export default defineConfig({
   build: {
     minify: true,
     outDir: "./dist",
     rollupOptions: {
-      input: ['components/progress/progress-circle/init-circle.js','components/progress/progress-steps/init-steps.js','components/progress/progress-bar/init-bar.js','components/progress/globals/classes/progress.js','components/progress/progress-bar/modules/classes/progress-bar.js', 'components/progress/progress-steps/modules/classes/progress-steps.js', 'components/progress/progress-circle/modules/classes/progress-circle.js'],
+      input: inputFileArr,
       output: {
         entryFileNames: `[name].js`,
       },
